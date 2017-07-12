db8f425de888d930b042e71aa8902aad00996fe4
vas <vas@softwareag.com>
2017-07-12 18:16:24 +0530

    value serializer added

--- hello-kafka/src/com/vag/kafka/Producer.java
+++ hello-kafka/src/com/vag/kafka/Producer.java
@@ -20,13 +20,16 @@ public class Producer {
       //Configure the Producer
       Properties configProperties = new Properties();
       configProperties.put("metadata.broker.list","192.168.17.128:9092");
-      configProperties.put("key.serializer.class","kafka.serializer.StringEncoder");
+      configProperties.put("key.serializer.class","com.vag.kafka.Encoder");
+      configProperties.put("serializer.class","com.vag.kafka.Encoder");
       configProperties.put("partitioner.class", "com.vag.kafka.SimplePartitioner");
+      configProperties.put("producer.type", "sync");
       ProducerConfig producerConfig = new ProducerConfig(configProperties);
-      kafka.javaapi.producer.Producer<byte[], String> producer = new kafka.javaapi.producer.Producer<byte[], String>(producerConfig);
+      kafka.javaapi.producer.Producer<String, String> producer = new kafka.javaapi.producer.Producer<String, String>(producerConfig);
+      System.out.println(producer);
       String line = in.nextLine();
       while(!line.equals("exit")) {
-          KeyedMessage<byte[], String> rec = new KeyedMessage<byte[], String>(topicName ,"nokey".getBytes(),line);
+          KeyedMessage<String, String> rec = new KeyedMessage<String, String>(topicName ,"nokey",line);
           producer.send(rec);
           
           line = in.nextLine();
